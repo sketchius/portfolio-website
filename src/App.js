a072dff4ae808643386305a12a4fd569cb9dca79
Bryce Huhtala <sketchius@gmail.com>
2022-11-26 13:54:53 -0500

    Fix typo

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
 					<div className='code-section'>
 						<code className='line-numbers'>
 							<p>1</p>
-							<p>3</p>
+							<p>2</p>
 							<p>3</p>
 							<p>4</p>
 							<p>5</p>
